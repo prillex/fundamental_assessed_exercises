c4ad3022f31cfa46969191abb13376c2e1b3c713
mattprill <s2057973@ed.ac.uk>
2024-11-05 20:19:52 +0000

    First nights Work

---
+++ throwaway_notes.R
@@ -0,0 +1,16 @@
+# Matt Prill
+# Fundamentals assessed exercises 
+# Throwaway Notes
+
+
+# Worse matrix presentation
+
+```{r}
+matrix_lambda <- matrix(c("1 - \u03BB", 1, 1,
+                          1, "10 - \u03BB", 4,
+                          1, 4, "10 - \u03BB"), nrow = 3)
+
+kable(matrix_lambda)
+
+
+```
